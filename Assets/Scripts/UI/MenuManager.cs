b052a5b701e2a80f8b551f9965e74ba4a83f56bf
Morgan Dunn <morgandunn21@gmail.com>
2020-11-10 22:30:25 -0600

    Now possible to choose host or client from custom menu

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -1,15 +1,26 @@
-﻿using System.Collections;
+﻿using Mirror;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    [Header("Network Manager")]
+    public NetworkManager manager;
+    [Header("Button Layouts")]
     public GameObject phoneButtons;
     public GameObject pcButtons;
     public GameObject devButtons;
+    [Header("Sub Menus")]
+    public GameObject joinScreen;
+    public TMP_InputField joinAddress;
+    
 
     private bool pcPlayer = true;
     private bool dev = false;
+    private GameObject mainScreen;
     // Start is called before the first frame update
     void Awake()
     {
@@ -22,36 +33,48 @@ public class MenuManager : MonoBehaviour
         {
             if (dev)
             {
-                devButtons.SetActive(true);
+                mainScreen = devButtons;
             }
             else
             {
-                pcButtons.SetActive(true);
+                mainScreen = pcButtons;
             }
         }
         else
         {
-            phoneButtons.SetActive(true);
+            mainScreen = phoneButtons;
         }
+
+        mainScreen.SetActive(true);
     }
 
     public void HostGame()
     {
+        manager.StartHost();
+    }
 
+    public void JoinGameScreen()
+    {
+        mainScreen.SetActive(!mainScreen.activeSelf);
+        joinScreen.SetActive(!joinScreen.activeSelf);
+        joinAddress.text = manager.networkAddress;
     }
 
     public void JoinGame()
     {
-
+        manager.networkAddress = joinAddress.text;
+        manager.StartClient();
     }
 
     public void OpenSettings()
     {
-
+        
     }
 
     public void PlayTutorial()
     {
 
     }
+
+
 }
