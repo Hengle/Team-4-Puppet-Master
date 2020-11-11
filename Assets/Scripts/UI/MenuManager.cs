b68164a1b436a9ac4dfef3fe2e653c2fe751d7fc
Morgan Dunn <morgandunn21@gmail.com>
2020-11-10 21:06:32 -0600

    Created Menu scene that chooses buttons based on build

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject phoneButtons;
+    public GameObject pcButtons;
+    public GameObject devButtons;
+
+    private bool pcPlayer = true;
+    private bool dev = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+#if UNITY_IOS || UNITY_ANDROID
+        pcPlayer = false;
+#elif UNITY_EDITOR
+        dev = true;
+#endif
+        if (pcPlayer)
+        {
+            if (dev)
+            {
+                devButtons.SetActive(true);
+            }
+            else
+            {
+                pcButtons.SetActive(true);
+            }
+        }
+        else
+        {
+            phoneButtons.SetActive(true);
+        }
+    }
+
+    public void HostGame()
+    {
+
+    }
+
+    public void JoinGame()
+    {
+
+    }
+
+    public void OpenSettings()
+    {
+
+    }
+
+    public void PlayTutorial()
+    {
+
+    }
+}
