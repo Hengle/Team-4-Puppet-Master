821e4a06744bcde3651ad2459bbf9897e496503a
1kingsam <123kingsam@gmail.com>
2021-03-29 23:24:03 -0500

    finalizing item spawn locations

--- Assets/Scripts/objectPlacement.cs
+++ Assets/Scripts/objectPlacement.cs
@@ -7,19 +7,29 @@ public class objectPlacement : MonoBehaviour
     public List<Vector3> winConditionLocationList;
     public List<Vector3> weaponLocationList;
     public List<Vector3> safeLocationList;
+    bool[] alreadyUsedLocation; 
+
     public GameObject bow, axe, usb, safe;
     public Transform itemsArea;
     // Start is called before the first frame update
     public void OnStartGame()
     {
-        GameObject addedBow=Instantiate(bow, itemsArea);
+        alreadyUsedLocation = new bool[winConditionLocationList.Count];
+
+        GameObject addedBow = Instantiate(bow, itemsArea);
         addedBow.transform.localPosition = weaponLocationList[Random.Range(0, weaponLocationList.Count)];
         GameObject addedAxe = Instantiate(axe, itemsArea);
         addedAxe.transform.localPosition = weaponLocationList[Random.Range(0, weaponLocationList.Count)];
-        for (int i=0; i<5; i++)
+        for (int i = 0; i < 5; i++)
         {
+            int rand = Random.Range(0, winConditionLocationList.Count);
+            while (alreadyUsedLocation[rand])
+                rand = Random.Range(0, winConditionLocationList.Count);
+
             GameObject addedUSB = Instantiate(usb, itemsArea);
-            addedUSB.transform.localPosition = winConditionLocationList[Random.Range(0, winConditionLocationList.Count)];
+            addedUSB.transform.localPosition = winConditionLocationList[rand];
+            alreadyUsedLocation[rand] = true;
+
         }
         GameObject addedSafe = Instantiate(safe, itemsArea);
         addedSafe.transform.localPosition = safeLocationList[Random.Range(0, safeLocationList.Count)];
